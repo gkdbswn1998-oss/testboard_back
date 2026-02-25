7a16221625313e424f1102829274be7f3fe73953
yunju <gkdbswn23238@naver.com>
2026-02-26 01:50:15 +0900

    "savePost 매서드 작성"

--- testBoard/src/main/java/com/example/testBoard/domain/controller/BoardController.java
+++ testBoard/src/main/java/com/example/testBoard/domain/controller/BoardController.java
@@ -3,19 +3,19 @@ package com.example.testBoard.domain.controller;
 import com.example.testBoard.domain.dto.BoardRequestDto;
 import com.example.testBoard.domain.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
+@CrossOrigin(origins = "http://localhost:3000")
 public class BoardController {
 
     private final BoardService boardService;
 
     @PostMapping("/save")
-    public void savePost(BoardRequestDto boardRequestDto){
-
+    public String savePost(@RequestBody BoardRequestDto boardRequestDto){
+        boardService.savePost(boardRequestDto);
+        return "게시글 등록 성공";
     }
 }
--- testBoard/src/main/java/com/example/testBoard/domain/repository/BoardRepository.java
+++ testBoard/src/main/java/com/example/testBoard/domain/repository/BoardRepository.java
@@ -1,6 +1,5 @@
 package com.example.testBoard.domain.repository;
 
-
 import com.example.testBoard.domain.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
